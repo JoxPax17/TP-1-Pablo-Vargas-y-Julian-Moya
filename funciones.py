5db3999e14c6e56d8715194ab4b1ccdb08bfe5c1
Pablo Vargas <joxpablo3011@gmail.com>
2026-05-03 23:16:09 -0600

    Añadi funciones

--- funciones.py
+++ funciones.py
@@ -4,4 +4,18 @@
 #Version: 3.14.3
 
 #Definicion de funciones:
-def 
+def crearListaPrueba():
+    listaDeTuplas = []
+    listaDeTuplas.append(("def", "FUNCION"))
+    listaDeTuplas.append(("if", "SI"))
+    listaDeTuplas.append(("return", "RETORNAR"))
+    return listaDeTuplas  
+def mostrarTokens (listaDeTuplas):
+    if len(listaDeTuplas)>0:
+        print ("--- Tokens cargados ---")
+        for tupla in listaDeTuplas:
+            palabra=tupla[0]
+            token=tupla[1]
+            print ("\n",palabra," es ahora ",token,"\n")
+    else:
+        print ("--- No hay tokens cargados ---")

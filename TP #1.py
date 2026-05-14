9bf7b33a0acda9c79d7f00d66941dd4ab0e8e838
Pablo Vargas <joxpablo3011@gmail.com>
2026-05-14 00:43:04 -0600

    Update TP #1.py

--- TP #1.py
+++ TP #1.py
@@ -1,12 +1,13 @@
 #Elaborado por: Pablo Vargas y Julian Moya
 #Fecha de creación: 01-05-26 10:00 am
-#Ultima modificacio: 13-05-26 03:30 pm
+#Ultima modificacio: 14-05-26 12:41 am
 #Version: 3.14.3
 import funciones
+listaTokens=[]
 def menu(listaTokens):
-    listaConteos = []
+    listaConteos=[]
+    listaBitacora= funciones.cargarBitacora()
     totalPalabras = 0
-    duracion = "0 segundos"
     while True:
         print("\n--- Menu Principal ---")
         print("1. Cargar tokens")
@@ -18,45 +19,29 @@ def menu(listaTokens):
         print("7. Generar HTML")
         print("8. Submenú de bitácora del sistema")
         print("9. Salir\n")
-        try:
-            opcion = int(input("Escoja una opcion: "))
-        except:
-            print("Opcion invalida")
-        if opcion == 1:
-            listaTokens = funciones.cargarArchivoTokens(listaTokens)
-            listaBitacora = funciones.registrarEvento(listaBitacora,"Se cargaron tokens desde archivo")
-        elif opcion == 2:
-            funciones.mostrarTokens(listaTokens)
-            listaBitacora = funciones.registrarEvento(listaBitacora,"Se mostraron los tokens")
-        elif opcion == 3:
-            listaTokens = funciones.agregarOModificarTokens(listaTokens)
-            listaBitacora = funciones.registrarEvento(listaBitacora,"Se agregaron/modificaron tokens")
-        elif opcion == 4:
-            funciones.guardarTokensEnArchivo(listaTokens)
-            listaBitacora = funciones.registrarEvento(listaBitacora,"Se guardaron tokens en archivo")
-        elif opcion == 5:
-            inicio = time.time()
-            listaConteos, totalPalabras = funciones.traducirCodigo(listaTokens)
-            fin = time.time()
-            duracion = str(round(fin - inicio, 2)) + " segundos"
-            listaBitacora = funciones.registrarEvento(listaBitacora,"Se tradujo un archivo de codigo")
-        elif opcion == 6:
-            funciones.generarCSV(listaConteos)
-            listaBitacora = funciones.registrarEvento(listaBitacora,"Se genero reporte CSV")
-        elif opcion == 7:
-            funciones.generarHTML(listaConteos, duracion, totalPalabras)
-            listaBitacora = funciones.registrarEvento(listaBitacora,"Se genero reporte HTML")
-        elif opcion == 8:
-            funciones.submenuBitacora(listaBitacora)
-            listaBitacora = funciones.registrarEvento(listaBitacora,"Se ingreso al submenu de bitacora")
-        elif opcion == 9:
-            listaBitacora = funciones.registrarEvento(listaBitacora,"El usuario salio del programa")
-            print("Programa finalizado")
-            break
+        opcion=int(input("Escoja una opción: "))
+        if opcion>=0 and opcion<=9:
+            if opcion==1:
+                listaTokens= funciones.cargarArchivoTokens(listaTokens)
+            elif opcion==2 :
+                funciones.mostrarTokens(listaTokens)
+            elif opcion==3:
+                listaTokens= funciones.agregarOModificarTokens(listaTokens)
+            elif opcion==4:
+                funciones.guardarTokensEnArchivo(listaTokens)
+            elif opcion==5:
+                listaConteos,totalPalabras= funciones.traducirCodigo(listaTokens)
+            elif opcion==6:
+                funciones.generarCSV(listaConteos)  
+            elif opcion==7: 
+                funciones.generarHTML(listaConteos,totalPalabras)
+            elif opcion==8:
+                funciones.submenuBitacora(listaBitacora)
+            elif opcion==9: 
+                break
         else:
-            print("Opcion invalida")
+            print ("\nOpcion invalida\n")
 
 #Programa principal
-listaTokens = []
-listaBitacora = funciones.cargarBitacora()
-menu(listaTokens, listaBitacora)
+listaTokens=[]
+menu(listaTokens)

e23a6684246823b6071cbff0bf8239c805d4a284
juliima <164700002+juliima@users.noreply.github.com>
2026-05-14 00:47:31 -0600

    Update TP #1.py

--- TP #1.py
+++ TP #1.py
@@ -3,45 +3,63 @@
 #Ultima modificacio: 14-05-26 12:41 am
 #Version: 3.14.3
 import funciones
-listaTokens=[]
-def menu(listaTokens):
-    listaConteos=[]
-    listaBitacora= funciones.cargarBitacora()
+import time
+def menu(listaTokens, listaBitacora):
+    listaConteos = []
     totalPalabras = 0
+    duracion = "0 segundos"
     while True:
         print("\n--- Menu Principal ---")
         print("1. Cargar tokens")
         print("2. Mostrar tokens")
         print("3. Agregar/modificar token")
         print("4. Guardar tokens")
-        print("5. Traducir código")
+        print("5. Traducir codigo")
         print("6. Generar CSV")
         print("7. Generar HTML")
-        print("8. Submenú de bitácora del sistema")
+        print("8. Submenu de bitacora")
         print("9. Salir\n")
-        opcion=int(input("Escoja una opción: "))
-        if opcion>=0 and opcion<=9:
-            if opcion==1:
-                listaTokens= funciones.cargarArchivoTokens(listaTokens)
-            elif opcion==2 :
-                funciones.mostrarTokens(listaTokens)
-            elif opcion==3:
-                listaTokens= funciones.agregarOModificarTokens(listaTokens)
-            elif opcion==4:
-                funciones.guardarTokensEnArchivo(listaTokens)
-            elif opcion==5:
-                listaConteos,totalPalabras= funciones.traducirCodigo(listaTokens)
-            elif opcion==6:
-                funciones.generarCSV(listaConteos)  
-            elif opcion==7: 
-                funciones.generarHTML(listaConteos,totalPalabras)
-            elif opcion==8:
-                funciones.submenuBitacora(listaBitacora)
-            elif opcion==9: 
-                break
-        else:
-            print ("\nOpcion invalida\n")
+        try:
+            opcion = int(input("Escoja una opcion: "))
+        except:
+            print("Opcion invalida")
+            continue
+        if opcion == 1:
+            listaTokens = funciones.cargarArchivoTokens(listaTokens)
 
-#Programa principal
-listaTokens=[]
-menu(listaTokens)
+            listaBitacora = funciones.registrarEvento(listaBitacora,"Se cargaron tokens desde archivo")
+        elif opcion == 2:
+            funciones.mostrarTokens(listaTokens)
+            listaBitacora = funciones.registrarEvento(listaBitacora,"Se mostraron los tokens")
+        elif opcion == 3:
+            listaTokens = funciones.agregarOModificarTokens(listaTokens)
+            listaBitacora = funciones.registrarEvento(listaBitacora,"Se agregaron/modificaron tokens")
+        elif opcion == 4:
+            funciones.guardarTokensEnArchivo(listaTokens)
+            listaBitacora = funciones.registrarEvento(listaBitacora,"Se guardaron tokens en archivo")
+        elif opcion == 5:
+            inicio = time.time()
+            listaConteos, totalPalabras = funciones.traducirCodigo(listaTokens)
+            fin = time.time()
+            duracion = str(round(fin - inicio, 2)) + " segundos"
+            listaBitacora = funciones.registrarEvento(listaBitacora,"Se tradujo un archivo de codigo")
+        elif opcion == 6:
+            funciones.generarCSV(listaConteos)
+            listaBitacora = funciones.registrarEvento(listaBitacora,"Se genero reporte CSV")
+        elif opcion == 7:
+            funciones.generarHTML(listaConteos, duracion, totalPalabras)
+            listaBitacora = funciones.registrarEvento(listaBitacora,"Se genero reporte HTML")
+        elif opcion == 8:
+            funciones.submenuBitacora(listaBitacora)
+            listaBitacora = funciones.registrarEvento(listaBitacora,"Se ingreso al submenu de bitacora")
+        elif opcion == 9:
+            listaBitacora = funciones.registrarEvento(listaBitacora,"El usuario salio del programa")
+            print("Programa finalizado")
+            break
+        else:
+            print("Opcion invalida")
+
+# Programa principal
+listaTokens = []
+listaBitacora = funciones.cargarBitacora()
+menu(listaTokens, listaBitacora)
